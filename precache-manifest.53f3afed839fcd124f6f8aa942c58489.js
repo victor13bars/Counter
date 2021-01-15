5dd0eb0d118326677dd2b891f9999ea39b08dccd
victor13bars <victor_bars_bars95@mail.ru>
2021-01-16 00:06:52 +0300

    Updates

---
+++ precache-manifest.53f3afed839fcd124f6f8aa942c58489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34fc638f0f62843875cd242996cb815",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "4989710ad37c9fa6ee29",
+    "url": "/Counter/static/css/main.82712620.chunk.css"
+  },
+  {
+    "revision": "730878e4811a85b92afc",
+    "url": "/Counter/static/js/2.a0c2208a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Counter/static/js/2.a0c2208a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4989710ad37c9fa6ee29",
+    "url": "/Counter/static/js/main.f1f06d91.chunk.js"
+  },
+  {
+    "revision": "acc9df7a62210d2be914",
+    "url": "/Counter/static/js/runtime-main.27a9aae1.js"
+  }
+]);
